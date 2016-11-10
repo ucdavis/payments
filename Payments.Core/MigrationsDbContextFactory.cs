8a6f2db666bd0c47755cf6c7514a321af6b8ff95
John Knoll <jpknoll@gmail.com>
2016-11-10 10:29:46 -0800

    factory for migrations tool to configure db connection

---
+++ Payments.Core/MigrationsDbContextFactory.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+
+namespace Payments.Core
+{
+    public class MigrationsDbContextFactory : IDbContextFactory<PaymentsContext>
+    {
+        public PaymentsContext Create(DbContextFactoryOptions options)
+        {
+            var builder = new DbContextOptionsBuilder<PaymentsContext>();
+
+            var connection = @"Server=.\sqlexpress;Database=Payments;Trusted_Connection=True;";
+            builder.UseSqlServer(connection);
+
+            return new PaymentsContext(builder.Options);
+        }
+    }
+}
