95b00dff4449ab41e628807fc50bf833e532647e
Scott Kirkland <srkirkland@gmail.com>
2016-11-22 15:45:28 -0800

    test project

---
+++ Payments.Tests/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Payments.Tests
+{
+    public class Class1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.Equal(4, 2 + 2);
+        }
+    }
+}
