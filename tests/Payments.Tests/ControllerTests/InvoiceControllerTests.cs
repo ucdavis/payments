5e1460e0b0dccd95c200dcacbabb8ff222529d67
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-11-23 11:31:34 -0800

    Not working yet, but added Moq

---
+++ tests/Payments.Tests/ControllerTests/InvoiceControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Internal;
+using Moq;
+using Payments.Core;
+using Payments.Controllers;
+using Payments.Core.Models;
+using Xunit;
+
+namespace Payments.Tests.ControllerTests
+{
+    public class InvoiceControllerTests
+    {
+        //private readonly Mock<PaymentsContext> _context;
+        //private readonly Mock<IMapper> _mapper;
+
+        //public InvoiceControllerTests()
+        //{
+
+        //   _context = new Mock<PaymentsContext>();
+        //   _mapper = new Mock<IMapper>();
+        //}
+
+        [Fact]
+        public void Test1()
+        {
+            //var xxx = new Mock<DbContextOptions<PaymentsContext>>();
+            var context = new Mock<PaymentsContext>();
+            //context.Setup
+            var mapper = new Mock<IMapper>();
+            var controller = new InvoiceController(context.Object, mapper.Object);
+        }
+
+    }
+}
