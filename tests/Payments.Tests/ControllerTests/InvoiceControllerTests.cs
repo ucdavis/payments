2988309668a9418031f96a72681380efb0b38337
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-11-28 11:36:35 -0800

    Tests

--- tests/Payments.Tests/ControllerTests/InvoiceControllerTests.cs
+++ tests/Payments.Tests/ControllerTests/InvoiceControllerTests.cs
@@ -7,10 +7,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Internal;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using Payments.Core;
 using Payments.Controllers;
 using Payments.Core.Models;
+using Payments.Mappings;
+using Payments.Models;
 using Payments.Tests.Helpers;
 using Xunit;
 
@@ -44,6 +47,41 @@ namespace Payments.Tests.ControllerTests
             var model = Assert.IsType<Invoice[]>(viewResult.Model);
             Assert.Equal(3, model.Count());
             Assert.Equal("Title2", model[1].Title);
+        }
+
+        [Fact]
+        public void TestCreateGetReturnView()
+        {
+            var mockContext = new Mock<PaymentsContext>();
+            var mapper = new Mock<IMapper>(); //Probably don't want to mock this. Maybe assign it directly?
+            var controller = new InvoiceController(mockContext.Object, mapper.Object);
+
+            var result = controller.Create();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        [Fact] //Test is failing need to fix
+        public async Task TestCreateAddsSomethin()
+        {
+
+            //IServiceCollection testServices = new ServiceCollection();
+            //testServices.AddAutoMapper(typeof(InvoiceMappingProfile));
+
+            //var xxx = testServices.Single(t => t.ServiceType == typeof(IMapper));
+            
+
+
+
+            var data = new InvoiceEditViewModel() {Title = "Jason", TotalAmount = 14.2m};
+
+            var mockContext = new Mock<PaymentsContext>();
+            var mapper = new Mock<IMapper>(); //Probably don't want to mock this. Maybe assign it directly?
+            mapper.Setup(a => a.Map<Invoice>(It.IsAny<InvoiceEditViewModel>())).Returns(CreateValidEntities.Invoice(5));
+            
+            
+            var controller = new InvoiceController(mockContext.Object, mapper.Object);
+
+            var result = await controller.Create(data);
         }
 
         private static Mock<DbSet<Invoice>> MockInvoice(IQueryable<Invoice> data)
