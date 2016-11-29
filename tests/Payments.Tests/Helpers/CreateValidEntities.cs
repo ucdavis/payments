5e47f807da285e6c18000b4b32572a08c37fe97c
Jason Sylvestre <jsylvestre@ucdavis.edu>
2016-11-29 08:15:13 -0800

    AutoMapperTests

--- tests/Payments.Tests/Helpers/CreateValidEntities.cs
+++ tests/Payments.Tests/Helpers/CreateValidEntities.cs
@@ -3,16 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Payments.Core.Models;
+using Payments.Models;
 
 namespace Payments.Tests.Helpers
 {
     public static class CreateValidEntities
     {
-        public static Invoice Invoice(int? counter)
+        public static Invoice Invoice(int? counter, bool populateAllFields = false)
         {
             var rtValue = new Invoice();
             rtValue.Title = string.Format("Title{0}", counter);
+            if (populateAllFields)
+            {
+                rtValue.TotalAmount = counter.HasValue? counter.Value: 99.9m;
+            }
             rtValue.Id = counter ?? 99;
+
+            return rtValue;
+        }
+
+        public static InvoiceEditViewModel InvoiceEditViewModel(int? counter, bool populateAllFields = false)
+        {
+            var rtValue = new InvoiceEditViewModel();
+            rtValue.Title = string.Format("Title{0}", counter);
+            if (populateAllFields)
+            {
+                rtValue.TotalAmount = counter.HasValue ? counter.Value : 99.9m;
+            }
 
             return rtValue;
         }
---
+++ tests/Payments.Tests/TestsMapper/AutoMapperTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Payments.Core.Models;
+using Payments.Mappings;
+using Payments.Models;
+using Payments.Tests.Helpers;
+using Shouldly;
+using Xunit;
+
+namespace Payments.Tests.TestsMapper
+{
+    public class AutoMapperTests
+    {
+        [Fact]
+        public void TestInvoiceEditViewModelMapsToInvoiceForTitle()
+        {
+            //Arrange
+            Mapper.Initialize(a => a.AddProfile(typeof(InvoiceMappingProfile)));
+            Mapper.Configuration.AssertConfigurationIsValid();
+            var source = CreateValidEntities.InvoiceEditViewModel(3, true);
+
+            //Act
+            var result = Mapper.Map<Invoice>(source);
+
+            //Assert
+            result.Title.ShouldBe("Title3");
+        }
+
+        [Fact]
+        public void TestInvoiceEditViewModelMapsToInvoiceForTotalAmount()
+        {
+            //Arrange
+            Mapper.Initialize(a => a.AddProfile(typeof(InvoiceMappingProfile)));
+            Mapper.Configuration.AssertConfigurationIsValid();
+            var source = CreateValidEntities.InvoiceEditViewModel(3, true);
+
+            //Act
+            var result = Mapper.Map<Invoice>(source);
+
+            //Assert
+            result.TotalAmount.ShouldBe(3.00m);
+        }
+
+        [Fact]
+        public void TestInvoiceMapsToInvoiceEditViewModelForTitle()
+        {
+            //Arrange
+            Mapper.Initialize(a => a.AddProfile(typeof(InvoiceMappingProfile)));
+            Mapper.Configuration.AssertConfigurationIsValid();
+            var source = CreateValidEntities.Invoice(3, true);
+
+            //Act
+            var result = Mapper.Map<InvoiceEditViewModel>(source);
+
+            //Assert
+            result.TotalAmount.ShouldBe(3.00m);
+        }
+
+        [Fact]
+        public void TestInvoiceMapsToInvoiceEditViewModelForTotalAmount()
+        {
+            //Arrange
+            Mapper.Initialize(a => a.AddProfile(typeof(InvoiceMappingProfile)));
+            Mapper.Configuration.AssertConfigurationIsValid();
+            var source = CreateValidEntities.Invoice(3, true);
+
+            //Act
+            var result = Mapper.Map<InvoiceEditViewModel>(source);
+
+            //Assert
+            result.TotalAmount.ShouldBe(3.00m);
+        }
+    }
+}
