7aac9dbcc7b9c4f2e5923c7da4ad77ecb091c678
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-18 15:35:48 -0700

    payments

--- src/Payments.Core/Domain/History.cs
+++ src/Payments.Core/Domain/History.cs
@@ -1,10 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Payments.Core.Domain
 {
-    class History
+    public class History
     {
+        [Key]
+        public int Id { get; set; }
+
+        public string Type { get; set; }
+
+        public string Data { get; set; }
+        public DateTime Date { get; set; }
+        public User Actor { get; set; }
+        public string ActorId { get; set; }
     }
 }
