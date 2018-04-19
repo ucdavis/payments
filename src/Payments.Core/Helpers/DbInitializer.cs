65ac402c0af5129f0bb5368d1aea28622a73b79a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-19 07:32:46 -0700

    Simple db init

---
+++ src/Payments.Core/Helpers/DbInitializer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Payments.Core.Data;
+using Payments.Core.Domain;
+
+namespace Payments.Core.Helpers
+{
+    public class DbInitializer
+    {
+        private readonly ApplicationDbContext _context;
+
+        public DbInitializer(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task RecreateAndInitialize()
+        {
+            await _context.Database.EnsureDeletedAsync();
+
+            await Initialize();
+        }
+
+        public async Task Initialize()
+        {
+            _context.Database.EnsureCreated();
+            //TODO: Revisit if users and roles change
+
+            var jason = new User
+            {
+                Id = "jason1",
+                Email = "jason@ucdavis.edu",
+                FirstName = "Jason",
+                LastName = "Sylvestre"
+            };
+            _context.Users.Add(jason);
+
+
+
+            await _context.SaveChangesAsync();
+
+        }
+    }
+}
