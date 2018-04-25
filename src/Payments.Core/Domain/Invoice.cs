376042f6e9eb2a87d379b2d94f7c6415e4d7befa
John Knoll <jpknoll@gmail.com>
2018-04-25 15:24:55 -0700

    payment dictionary method

--- src/Payments.Core/Domain/Invoice.cs
+++ src/Payments.Core/Domain/Invoice.cs
@@ -53,5 +53,34 @@ namespace Payments.Core.Domain
                 .WithMany()
                 .OnDelete(DeleteBehavior.Restrict);
         }
+
+        public Dictionary<string, string> GetPaymentDictionary()
+        {
+            var dictionary = new Dictionary<string, string>
+            {
+                {"transaction_type"       , "sale"},
+                {"reference_number"       , Id.ToString()},
+                {"amount"                 , Total.ToString("F2")},
+                {"currency"               , "USD"},
+                {"transaction_uuid"       , Guid.NewGuid().ToString()},
+                {"signed_date_time"       , DateTime.UtcNow.ToString("yyyy-MM-dd'T'HH:mm:ss'Z'")},
+                {"unsigned_field_names"   , string.Empty},
+                {"locale"                 , "en"},
+                {"bill_to_email"          , CustomerEmail},
+                {"bill_to_forename"       , CustomerName},
+                {"bill_to_address_country", "US"},
+                {"bill_to_address_state"  , "CA"}
+            };
+
+            dictionary.Add("line_item_count", Items.Count.ToString("D"));
+            for (var i = 0; i < Items.Count; i++)
+            {
+                dictionary.Add($"item_{i}_name", Items[i].Description);
+                dictionary.Add($"item_{i}_quantity", Items[i].Quantity.ToString());
+                dictionary.Add($"item_{i}_unit_price", Items[i].Amount.ToString("F2"));
+            }
+
+            return dictionary;
+        }
     }
 }
