4ceb6ab7019243fa413a6eb2a8b23ba2c6e900b7
Jason Sylvestre <jsylvestre@ucdavis.edu>
2018-04-30 11:21:07 -0700

    Allow not to rebuild the db

--- src/Payments.Mvc/Settings.cs
+++ src/Payments.Mvc/Settings.cs
@@ -5,5 +5,6 @@ namespace Payments.Mvc
         public string CasBaseUrl { get; set; }
         public string IetWsKey { get; set; }
         public string FinancialLookupUrl { get; set; }
+        public string RebuildDb { get; set; } = "Yes";
     }
 }
