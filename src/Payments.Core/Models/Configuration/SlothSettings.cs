bb8b110e50e10d1066eccc63e60e085e7fee7c81
John Knoll <jpknoll@gmail.com>
2018-04-30 10:18:42 -0700

    sloth client

---
+++ src/Payments.Core/Models/Configuration/SlothSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payments.Core.Models.Configuration
+{
+    public class SlothSettings
+    {
+        public string BaseUrl { get; set; }
+        public string ApiKey { get; set; }
+    }
+}
