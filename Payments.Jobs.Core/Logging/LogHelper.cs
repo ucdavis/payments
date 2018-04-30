341212eeb1d10ccb1027306fd2d0db7a20d67af6
John Knoll <jpknoll@gmail.com>
2018-04-30 10:18:03 -0700

    core jobs project

---
+++ Payments.Jobs.Core/Logging/LogHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+
+namespace Payments.Jobs.Core.Logging
+{
+    public class LogHelper
+    {
+        public static void ConfigureLogging(IConfigurationRoot configuration)
+        {
+        }
+    }
+}
