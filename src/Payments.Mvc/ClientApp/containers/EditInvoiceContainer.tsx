32d96cb70de5d30b417c00dc558e29b6a0489e9e
John Knoll <jpknoll@gmail.com>
2018-06-05 10:46:14 -0700

    move items table to component

--- src/Payments.Mvc/ClientApp/containers/EditInvoiceContainer.tsx
+++ src/Payments.Mvc/ClientApp/containers/EditInvoiceContainer.tsx
@@ -4,10 +4,9 @@ import { Invoice } from '../models/Invoice';
 import { InvoiceCustomer } from '../models/InvoiceCustomer';
 import { InvoiceItem } from '../models/InvoiceItem';
 
-import DiscountInput from '../components/discountInput';
+import EditItemsTable from '../components/editItemsTable';
 import LoadingModal from '../components/loadingModal';
 import MemoInput from '../components/memoInput';
-import TaxInput from '../components/taxInput';
 
 declare var antiForgeryToken: string;
 
@@ -22,12 +21,7 @@ interface IState {
     discount: number;
     taxRate: number;
     memo: string;
-    items: {
-        byId: number[];
-        byHash: {
-            [key: number]: InvoiceItem;
-        };
-    };
+    items: InvoiceItem[];
     loading: boolean;
     errorMessage: string;
 }
@@ -38,27 +32,16 @@ export default class EditInvoiceContainer extends React.Component<IProps, IState
 
         const { invoice } = this.props;
 
-        // map array to object
-        const items: IState["items"] = {
-            byHash: {},
-            byId: [],
-        };
-
         // require at least one item
-        if (!invoice.items || invoice.items.length < 1) {
-            invoice.items = [{
+        const items = invoice.items;
+        if (!items || items.length < 1) {
+            items.push({
                 amount: 0,
                 description: '',
                 id: 0,
                 quantity: 0,
-            }];
+            });
         }
-
-        props.invoice.items.forEach((item, index) => {
-            const id = item.id;
-            items.byId.push(id);
-            items.byHash[id] = item;
-        });
 
         this.state = {
             customer: {
@@ -78,13 +61,11 @@ export default class EditInvoiceContainer extends React.Component<IProps, IState
     public render() {
         const { invoice } = this.props;
         const { customer, items, discount, taxRate, memo, loading } = this.state;
-        const subtotal = this.calculateSubTotal();
-        const tax = this.calculateTaxAmount();
-        const total = this.calculateTotal();
+        
 
         return (
             <div className="card-style">
-                <LoadingModal isOpen={loading} />
+                <LoadingModal loading={loading} />
                 <div className="card-header-yellow card-bot-border">
                     <div className="card-head">
                         <h2>Edit Invoice #{ invoice.id }</h2>
@@ -105,56 +86,14 @@ export default class EditInvoiceContainer extends React.Component<IProps, IState
                 </div>
                 <div className="card-content invoice-items">
                     <h3>Invoice Items</h3>
-                    <table className="table invoice-table">
-                        <thead>
-                            <tr>
-                                <th>Description</th>
-                                <th>Qty</th>
-                                <th>Price</th>
-                                <th>Amount</th>
-                                <th/>
-                            </tr>
-                        </thead>
-                        <tbody className="tbody-invoice-details">
-                            { items.byId.map((id) => this.renderItem(id, items.byHash[id])) }
-                        </tbody>
-                        <tbody>
-                            <tr>
-                                <td>
-                                    <button className="btn-plain primary-color" onClick={this.createNewItem}>
-                                        <i className="fa fa-plus" /> Add another item
-                                    </button>
-                                </td>
-                                <td>Subtotal</td>
-                                <td />
-                                <td>${ subtotal.toFixed(2) }</td>
-                                <td />
-                            </tr>
-                            <tr>
-                                <td />
-                                <td>Discount</td>
-                                <td><DiscountInput value={discount} onChange={(v) => this.updateProperty('discount', v)} /></td>
-                                <td>$({ (Number(discount)).toFixed(2) })</td>
-                                <td />
-                            </tr>
-                            <tr>
-                                <td />
-                                <td>Tax</td>
-                                <td><TaxInput value={taxRate} onChange={(v) => this.updateProperty('taxRate', v)} /></td>
-                                <td>${ tax.toFixed(2) }</td>
-                                <td />
-                            </tr>
-                        </tbody>
-                        <tfoot>
-                            <tr>
-                                <td />
-                                <td>Total</td>
-                                <td />
-                                <td>${ total.toFixed(2) }</td>
-                                <td />
-                            </tr>
-                        </tfoot>
-                    </table>
+                    <EditItemsTable
+                        items={items}
+                        discount={discount}
+                        taxRate={taxRate}
+                        onItemsChange={(v) => this.updateProperty('items', v)}
+                        onDiscountChange={(v) => this.updateProperty('discount', v)}
+                        onTaxRateChange={(v) => this.updateProperty('taxRate', v)}
+                    />
                 </div>
                 <div className="card-content invoice-memo">
                     <h3>Memo</h3>
@@ -172,64 +111,12 @@ export default class EditInvoiceContainer extends React.Component<IProps, IState
                         </div>
                         <div className="col d-flex justify-content-end align-items-center">
                             <button className="btn-plain" onClick={this.onSubmit}>Save and close</button>
-                            { invoice.sent &&
+                            { !invoice.sent &&
                                 <button className="btn" onClick={this.onSend}>Send</button> }
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-
-    private renderItem(id: number, item: InvoiceItem) {
-        const { description, quantity, amount } = item;
-
-        return (
-            <tr key={id}>
-                <td>
-                    <input
-                        type="text"
-                        className="form-control"
-                        placeholder=""
-                        value={description}
-                        onChange={(e) => { this.updateItemProperty(id, 'description', e.target.value) }}
-                    />
-                </td>
-                <td>
-                    <input
-                        type="number"
-                        min="0"
-                        className="form-control"
-                        placeholder="0"
-                        value={quantity}
-                        onChange={(e) => { this.updateItemProperty(id, 'quantity', e.target.value) }}
-                    />
-                </td>
-                <td>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <span className="input-group-text">$</span>
-                        </div>
-                        <input
-                            type="number"
-                            min="0"
-                            step="0.01"
-                            className="form-control"
-                            placeholder="0.00"
-                            value={amount}
-                            onChange={(e) => { this.updateItemProperty(id, 'amount', e.target.value) }}
-                        />
-                    </div>
-                </td>
-                <td>
-                    ${ (quantity * amount).toFixed(2) }
-                </td>
-                <td>
-                    <button className="btn-invoice-delete" onClick={() => this.removeItem(id)}>
-                        <i className="fa fa-times" />
-                    </button>
-                </td>
-            </tr>
         );
     }
 
@@ -255,99 +142,15 @@ export default class EditInvoiceContainer extends React.Component<IProps, IState
         });
     }
 
-    private createNewItem = () => {
-        const items = this.state.items;
-        // needs new id logic
-        const id = items.byId.reduce((max, value) => Math.max(max, value), 0) + 1;
-        this.setState({
-            items: {
-                byHash: {
-                    ...items.byHash,
-                    [id]: {
-                        amount: 0,
-                        description: '',
-                        id,
-                        quantity: 0,
-                    },
-                },
-                byId: [...items.byId, id],
-            },
-        });
-    }
-
-    private removeItem = (id) => {
-        const items = this.state.items;
-        const newHash = {...items.byHash};
-        delete newHash[id];
-
-        const newItems = {
-            byHash: newHash,
-            byId: items.byId.filter(i => i !== id),
-        };
-
-        this.setState({
-            items: newItems,
-        });
-
-        // if this would set the list empty, add an empty one back
-        if (newItems.byId.length < 1) {
-            this.createNewItem();
-        }
-    }
-
-    private updateItem = (id, item) => {
-        const items = this.state.items;
-        const newHash = {...items.byHash};
-        newHash[id] = item;
-
-        this.setState({
-            items: {
-                byHash: newHash,
-                byId: items.byId,
-            },
-        });
-    }
-
-    private updateItemProperty = (id, name, value) => {
-        const item = this.state.items.byHash[id];
-        item[name] = value;
-        this.updateItem(id, item);
-    }
-
-    private calculateSubTotal = () => {
-        const items = this.state.items;
-        const sum = items.byId.reduce((prev, id) => {
-            const item = items.byHash[id];
-            return prev + (item.quantity * item.amount);
-        }, 0);
-
-        return sum;
-    }
-
-    private calculateTaxAmount = () => {
-        const { discount, taxRate } = this.state;
-        const sub = this.calculateSubTotal();
-        return (sub - discount) * taxRate;
-    }
-
-    private calculateTotal = () => {
-        const { discount } = this.state;
-        const sub = this.calculateSubTotal();
-        const tax = this.calculateTaxAmount();
-        return sub - discount + tax;
-    }
-
     private saveInvoice = async () => {
         const { id } = this.props.invoice;
         const { customer, discount, taxRate, items, memo } = this.state;
-
-        const invoiceItems = items.byId.map(itemId => items.byHash[itemId]);
 
         // create submit object
         const invoice = {
             customer,
             discount,
-            items: invoiceItems,
+            items,
             memo,
             tax: taxRate,
         };
