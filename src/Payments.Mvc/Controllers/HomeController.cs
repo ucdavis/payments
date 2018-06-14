bccbea82b120f7a7925d2bf7d0a1cd209fac1174
John Knoll <jpknoll@gmail.com>
2018-06-14 16:25:59 -0700

    team selector

--- src/Payments.Mvc/Controllers/HomeController.cs
+++ src/Payments.Mvc/Controllers/HomeController.cs
@@ -1,15 +1,21 @@
 using System;
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Payments.Core.Data;
 using Payments.Mvc.Identity;
 
 namespace Payments.Mvc.Controllers
 {
     public class HomeController : SuperController
     {
-        public HomeController(ApplicationUserManager userManager) : base(userManager)
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ApplicationUserManager userManager, ApplicationDbContext context) : base(userManager)
         {
+            _context = context;
         }
 
         public IActionResult Index()
@@ -26,6 +32,24 @@ namespace Payments.Mvc.Controllers
         [Authorize]
         public IActionResult Secure() {
             return Content("Logged in");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SetActiveTeam(int teamId)
+        {
+            var team = await _context.Teams
+                .FirstOrDefaultAsync(t => t.Id == teamId);
+
+            // TODO: Check permissions
+
+            // TODO: Save as default team
+
+            if (team == null)
+            {
+                return RedirectToAction(nameof(Index), "Home");
+            }
+
+            return RedirectToAction(nameof(Index), "Home", new { team = team.Slug });
         }
     }
 }
