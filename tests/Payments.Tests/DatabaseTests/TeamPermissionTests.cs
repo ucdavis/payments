463eb09994f72168ec034dc3618247f1b4393e28
John Knoll <jpknoll@gmail.com>
2018-06-18 10:54:01 -0700

    move projects

---
+++ tests/Payments.Tests/DatabaseTests/TeamPermissionTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Payments.Core.Domain;
+using TestHelpers.Helpers;
+using Xunit;
+
+namespace Payments.Tests.DatabaseTests
+{
+    [Trait("Category", "DatabaseTests")]
+    public class TeamPermissionTests
+    {
+        [Fact]
+        public void TestAllFieldsInTheDatabaseHaveBeenTested()
+        {
+            #region Arrange
+            var expectedFields = new List<NameAndType>();
+
+            expectedFields.Add(new NameAndType("Id", "System.Int32", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.KeyAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("Role", "Payments.Core.Domain.TeamRole", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("RoleId", "System.Int32", new List<string>()));
+            expectedFields.Add(new NameAndType("Team", "Payments.Core.Domain.Team", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("TeamId", "System.Int32", new List<string>()));
+            expectedFields.Add(new NameAndType("User", "Payments.Core.Domain.User", new List<string>
+            {
+                "[System.ComponentModel.DataAnnotations.RequiredAttribute()]",
+            }));
+            expectedFields.Add(new NameAndType("UserId", "System.String", new List<string>()));
+
+            #endregion Arrange
+
+            AttributeAndFieldValidation.ValidateFieldsAndAttributes(expectedFields, typeof(TeamPermission));
+        }
+    }
+}
