060d66035e3acfecb64c154c5552bb8cd8403d5b
John Knoll <jpknoll@gmail.com>
2018-06-22 11:56:08 -0700

    new account select component

---
+++ src/Payments.Mvc/ClientApp/components/accountSelectControl.tsx
@@ -0,0 +1,159 @@
+import * as React from 'react';
+
+import { Account } from '../models/Account';
+
+import Modal from './modal';
+
+interface IProps {
+    accounts: Account[];
+    value: number;
+    onChange: (value: number) => void;
+}
+
+interface IState {
+    isModalOpen: boolean;
+}
+
+function sortAccounts(a, b) {
+    if (a.isDefault) {
+        return -1;
+    }
+
+    if (b.isDefault) {
+        return 1;
+    }
+
+    if (a.name < b.name) {
+        return -1;
+    }
+
+    if (a.name > b.name) {
+        return 1;
+    }
+
+    return 0;
+}
+
+export default class AccountSelectControl extends React.Component<IProps, IState> {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isModalOpen: false,
+        };
+    }
+
+    public render() {
+        const { accounts, value, onChange } = this.props;
+
+        const ordered = [...accounts].sort(sortAccounts);
+
+        return (
+            <div className="input-group">
+                <select className="custom-select" value={value} onChange={(e) => onChange(Number(e.target.value))}>
+                    { ordered.map(a => <option key={a.id} value={a.id}>{a.name}</option>) }
+                </select>
+                <div className="input-group-append">
+                    <button className="btn btn-outline-secondary" type="button" onClick={this.openModal}>
+                        <i className="fa fa-info-circle" />
+                    </button>
+                </div>
+                {this.renderModal()}
+            </div>
+        );
+    }
+
+    private renderModal() {
+        const { accounts, value } = this.props;
+        const { isModalOpen } = this.state;
+
+        const ordered = [...accounts].sort(sortAccounts);
+
+        return (
+            <Modal dialogClassName="account-details-modal modal-lg" isOpen={isModalOpen} onBackdropClick={this.closeModal} onEscape={this.closeModal}>
+                <div className="modal-header">
+                    <div className="row flex-grow-1">
+                        <div className="col-md-3" />
+                        <div className="col-md-6 d-flex justify-content-center">
+                            <span className="modal-title">Accounts Details</span>
+                        </div>
+                        <div className="col-md-3 d-flex justify-content-end align-items-start">
+                            <button type="button" className="close" onClick={this.closeModal}>
+                                <span aria-hidden="true"><i className="fa fa-fw fa-times" /> Close</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                { ordered.map(this.renderAccountDetails) }
+            </Modal>
+        );
+    }
+
+    private renderAccountDetails = (item: Account) => {
+        const selectedValue = this.props.value;
+        const { 
+            id, name, description, isDefault,
+            chart, account, subAccount, object, subObject, project
+        } = item;
+
+        let accountString = `${chart}-${account}`;
+        if (subAccount) {
+            accountString += `-${subAccount}`;
+        }
+
+        let objectString = object;
+        if (subObject) {
+            objectString += `-${subObject}`;
+        }
+
+        return (
+            <div className="modal-body" key={id}>
+                <div className="row flex-grow-1">
+                    <div className="col-md-4">
+                        <span className="account-name">{name}</span>
+                        <p className="account-description">{description}</p>
+                    </div>
+                    <div className="col-md-4 d-flex flex-column">
+                        <dl className="row">
+                            <dt className="col-sm-4">Account</dt>
+                            <dd className="col-sm-8">{accountString}</dd>
+
+                            <dt className="col-sm-4">Object</dt>
+                            <dd className="col-sm-8">{objectString}</dd>
+
+                            <dt className="col-sm-4">Project</dt>
+                            <dd className="col-sm-8">{project}</dd>
+                        </dl>
+                    </div>
+                    <div className="col-md-3 d-flex flex-column justify-content-around align-items-center">
+                    { isDefault &&
+                        <span className="badge badge-info">default</span>
+                    }
+                    { selectedValue === id &&
+                        <span className="badge badge-success">selected</span>
+                    }
+                    </div>
+                    <div className="col-md-1 d-flex justify-content-end align-items-center">
+                        <button className="btn" onClick={() => this.modalPickAccount(id)}>
+                            <i className="fa fa-share" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+
+    private openModal = () => {
+        this.setState({ isModalOpen: true });
+    }
+
+    private closeModal = () => {
+        this.setState({ isModalOpen: false });
+    }
+
+    private modalPickAccount = (id: number) => {
+        this.props.onChange(id);
+        this.closeModal();
+    }
+}
