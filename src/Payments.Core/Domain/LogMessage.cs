8ca96ee73b097731a25d06866469645386df9ef1
John Knoll <jpknoll@gmail.com>
2018-07-30 14:05:43 -0700

    New logs table

---
+++ src/Payments.Core/Domain/LogMessage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+
+namespace Payments.Core.Domain
+{
+    public class LogMessage
+    {
+        public int Id { get; set; }
+
+        public string Source { get; set; }
+
+        public string Message { get; set; }
+
+        public string MessageTemplate { get; set; }
+
+        public string Level { get; set; }
+
+        public DateTimeOffset TimeStamp { get; set; }
+
+        public string Exception { get; set; }
+
+        public string Properties { get; set; }
+
+        public string LogEvent { get; set; }
+
+        public string CorrelationId { get; set; }
+
+        public string JobId { get; set; }
+
+        public string JobName { get; set; }
+
+        public static void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<LogMessage>()
+                .ToTable("Logs");
+
+            modelBuilder.Entity<LogMessage>()
+                .Property(l => l.Properties)
+                .HasColumnType("xml");
+        }
+    }
+}
