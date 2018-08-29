43e9ae4f9f66938b12dba479f114db9cb1fc373a
John Knoll <jpknoll@gmail.com>
2018-08-29 11:47:13 -0700

    empty controllers for later

---
+++ src/Payments.Mvc/Controllers/ReportsController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Payments.Mvc.Controllers
+{
+    public class ReportsController : SuperController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
