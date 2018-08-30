113afa0d601105e36f17117323ce45b4963e5b6a
John Knoll <jpknoll@gmail.com>
2018-08-30 13:07:14 -0700

    empty controller

---
+++ src/Payments.Mvc/Controllers/SearchController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Payments.Mvc.Controllers
+{
+    public class SearchController : SuperController
+    {
+        public IActionResult Query(string q)
+        {
+            return View();
+        }
+    }
+}
