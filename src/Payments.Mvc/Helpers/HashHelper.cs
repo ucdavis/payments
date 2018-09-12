f19ad0158d9ea86a8cee276b76220dac75ecf414
John Knoll <jpknoll@gmail.com>
2018-09-12 09:56:05 -0700

    add google analytics
    with user id

---
+++ src/Payments.Mvc/Helpers/HashHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Payments.Mvc.Helpers
+{
+    public static class HashHelper
+    {
+        public static string GetSHA256Hash(this string input)
+        {
+            var alg = SHA256.Create();
+            var hash = alg.ComputeHash(Encoding.UTF8.GetBytes(input));
+
+            var sb = new StringBuilder();
+            foreach (var b in hash)
+            {
+                sb.Append(b.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+    }
+}
