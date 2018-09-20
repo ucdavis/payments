bd5e41dac3a2f6e941b2599dc91f09e44877794b
John Knoll <jpknoll@gmail.com>
2018-09-20 16:03:11 -0700

    add outlook regex
    for later :-D

--- src/Payments.Mvc/ClientApp/components/multiCustomerControl.tsx
+++ src/Payments.Mvc/ClientApp/components/multiCustomerControl.tsx
@@ -7,6 +7,8 @@ import EditCustomerModal from './editCustomerModal';
 
 const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
+const outlookRegex = /(?:"?([^"]*)"?\s)?(?:<?(.+@[^>]+)>?)/;
+
 interface IProps {
     customers: InvoiceCustomer[];
     onChange: (value: InvoiceCustomer[]) => void;
