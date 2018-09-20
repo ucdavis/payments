10c4356f3115b23dde153ad647fd76fe4244e753
John Knoll <jpknoll@gmail.com>
2018-09-20 11:12:11 -0700

    use try/catch

--- src/Payments.Core/Jobs/MoneyMovementJob.cs
+++ src/Payments.Core/Jobs/MoneyMovementJob.cs
@@ -33,87 +33,96 @@ namespace Payments.Core.Jobs
 
         public async Task FindBankReconcileTransactions(ILogger log)
         {
-            // get all invoices that are waiting for reconcile
-            var invoices = _dbContext.Invoices
-                .Where(i => i.Status == Invoice.StatusCodes.Paid)
-                .Include(i => i.Payment)
-                .Include(i => i.Team)
-                    .ThenInclude(t => t.Accounts)
-                .ToList();
-
-            log.Information("{count} invoices found expecting reconciliation", invoices.Count);
-
-            foreach (var invoice in invoices)
+            try
             {
-                var transaction = await _slothService.GetTransactionsByProcessorId(invoice.Payment.Transaction_Id);
-                if (transaction == null)
-                {
-                    log.Warning("No reconcilation found for invoice id: {id}", invoice.Id);
-                    continue;
-                };
+                // get all invoices that are waiting for reconcile
+                var invoices = _dbContext.Invoices
+                    .Where(i => i.Status == Invoice.StatusCodes.Paid)
+                    .Include(i => i.Payment)
+                    .Include(i => i.Team)
+                        .ThenInclude(t => t.Accounts)
+                    .ToList();
 
-                log.Information("Invoice {id} reconciliation found with transaction: {transactionId}", invoice.Id, transaction.Id);
+                log.Information("{count} invoices found expecting reconciliation", invoices.Count);
 
-                // get team account info
-                var team = invoice.Team;
-                if (team.DefaultAccount == null)
+                foreach (var invoice in invoices)
                 {
-                    log.Warning("Team {team} has no default account for payments", team.Name);
-                    continue;
+                    var transaction = await _slothService.GetTransactionsByProcessorId(invoice.Payment.Transaction_Id);
+                    if (transaction == null)
+                    {
+                        log.Warning("No reconcilation found for invoice id: {id}", invoice.Id);
+                        continue;
+                    };
+
+                    log.Information("Invoice {id} reconciliation found with transaction: {transactionId}", invoice.Id, transaction.Id);
+
+                    // get team account info
+                    var team = invoice.Team;
+                    if (team.DefaultAccount == null)
+                    {
+                        log.Warning("Team {team} has no default account for payments", team.Name);
+                        continue;
+                    }
+
+                    // transaction found, bank reconcile was successful
+                    invoice.Status = Invoice.StatusCodes.Processing;
+
+                    // calculate fees
+                    var feeAmount = invoice.Total * FeeSchedule.StandardRate;
+                    var incomeAmount = invoice.Total - feeAmount;
+
+                    // create transfers
+                    var debitHolding = new CreateTransfer()
+                    {
+                        Amount    = invoice.Total,
+                        Direction = Transfer.CreditDebit.Debit,
+                        Chart     = _financeSettings.ClearingChart,
+                        Account   = _financeSettings.ClearingAccount,
+                    };
+
+                    var feeCredit = new CreateTransfer()
+                    {
+                        Amount    = feeAmount,
+                        Direction = Transfer.CreditDebit.Credit,
+                        Chart     = _financeSettings.FeeChart,
+                        Account   = _financeSettings.FeeAccount,
+                    };
+
+                    var incomeCredit = new CreateTransfer()
+                    {
+                        Amount        = incomeAmount,
+                        Direction     = Transfer.CreditDebit.Credit,
+                        Chart         = team.DefaultAccount.Chart,
+                        Account       = team.DefaultAccount.Account,
+                        SubAccount    = team.DefaultAccount.SubAccount,
+                        ObjectCode    = team.DefaultAccount.Object,
+                        SubObjectCode = team.DefaultAccount.SubObject,
+                    };
+
+                    var response = await _slothService.CreateTransaction(new CreateTransaction()
+                    {
+                        AutoApprove            = false,
+                        MerchantTrackingNumber = transaction.MerchantTrackingNumber,
+                        TransactionDate        = DateTime.UtcNow,
+                        Transfers              = new List<CreateTransfer>()
+                        {
+                            debitHolding,
+                            feeCredit,
+                            incomeCredit,
+                        },
+                    });
+
+                    log.Information("Transaction created with ID: {id}", response.Id);
                 }
 
-                // transaction found, bank reconcile was successful
-                invoice.Status = Invoice.StatusCodes.Processing;
-
-                // calculate fees
-                var feeAmount = invoice.Total * FeeSchedule.StandardRate;
-                var incomeAmount = invoice.Total - feeAmount;
-
-                // create transfers
-                var debitHolding = new CreateTransfer()
-                {
-                    Amount    = invoice.Total,
-                    Direction = Transfer.CreditDebit.Debit,
-                    Chart     = _financeSettings.ClearingChart,
-                    Account   = _financeSettings.ClearingAccount,
-                };
-
-                var feeCredit = new CreateTransfer()
-                {
-                    Amount    = feeAmount,
-                    Direction = Transfer.CreditDebit.Credit,
-                    Chart     = _financeSettings.FeeChart,
-                    Account   = _financeSettings.FeeAccount,
-                };
-
-                var incomeCredit = new CreateTransfer()
-                {
-                    Amount        = incomeAmount,
-                    Direction     = Transfer.CreditDebit.Credit,
-                    Chart         = team.DefaultAccount.Chart,
-                    Account       = team.DefaultAccount.Account,
-                    SubAccount    = team.DefaultAccount.SubAccount,
-                    ObjectCode    = team.DefaultAccount.Object,
-                    SubObjectCode = team.DefaultAccount.SubObject,
-                };
-
-                var response = await _slothService.CreateTransaction(new CreateTransaction()
-                {
-                    AutoApprove            = false,
-                    MerchantTrackingNumber = transaction.MerchantTrackingNumber,
-                    TransactionDate        = DateTime.UtcNow,
-                    Transfers = new List<CreateTransfer>()
-                    {
-                        debitHolding,
-                        feeCredit,
-                        incomeCredit,
-                    },
-                });
-
-                log.Information("Transaction created with ID: {id}", response.Id);
+                log.Information("Finishing Job");
+                await _dbContext.SaveChangesAsync();
             }
-
-            await _dbContext.SaveChangesAsync();
+            catch (Exception ex)
+            {
+                log.Error(ex, ex.Message);
+                throw;
+            }
         }
     }
 }
