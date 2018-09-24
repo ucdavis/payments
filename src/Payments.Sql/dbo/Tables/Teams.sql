61ee23d101b1c8688cb56058c426003e18c065b5
John Knoll <jpknoll@gmail.com>
2018-09-24 15:03:34 -0700

    set default value

--- src/Payments.Sql/dbo/Tables/Teams.sql
+++ src/Payments.Sql/dbo/Tables/Teams.sql
@@ -1,12 +1,14 @@
 ﻿CREATE TABLE [dbo].[Teams] (
-    [Id]       INT            IDENTITY (1, 1) NOT NULL,
-    [IsActive] BIT            NOT NULL,
-    [Name]     NVARCHAR (128) NOT NULL,
-    [Slug]     NVARCHAR (40)  NOT NULL,
-    [ContactName] NVARCHAR(128) NULL, 
-    [ContactEmail] NVARCHAR(128) NULL, 
-    [ContactPhoneNumber] NVARCHAR(40) NULL, 
-    [ApiKey] NVARCHAR(50) NULL, 
+    [Id]                 INT            IDENTITY (1, 1) NOT NULL,
+    [IsActive]           BIT            NOT NULL,
+    [Name]               NVARCHAR (128) NOT NULL,
+    [Slug]               NVARCHAR (40)  NOT NULL,
+    [ContactName]        NVARCHAR (128) NULL,
+    [ContactEmail]       NVARCHAR (128) NULL,
+    [ContactPhoneNumber] NVARCHAR (40)  NULL,
+    [ApiKey]             NVARCHAR (50)  CONSTRAINT [DF_Teams_ApiKey] DEFAULT (replace(newid(),'-','')) NOT NULL,
     CONSTRAINT [PK_Teams] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
+
+
 
