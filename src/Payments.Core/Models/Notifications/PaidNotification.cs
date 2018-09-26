1c1e1750492a16a4ce091dbc18e7a7a41119d801
John Knoll <jpknoll@gmail.com>
2018-09-26 11:59:50 -0700

    new notification type

---
+++ src/Payments.Core/Models/Notifications/PaidNotification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Payments.Core.Models.Notifications
+{
+    public class PaidNotification
+    {
+        public static string Name = "invoice_paid";
+
+        public int InvoiceId { get; set; }
+    }
+}
