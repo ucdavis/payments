0117b85d3021534b06569085fbe0b11a1942350c
John Knoll <jpknoll@gmail.com>
2018-10-02 15:54:51 -0700

    add search results

--- src/Payments.Mvc/Controllers/SearchController.cs
+++ src/Payments.Mvc/Controllers/SearchController.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Payments.Core.Data;
+using Payments.Mvc.Models.SearchViewModels;
 
 namespace Payments.Mvc.Controllers
 {
     public class SearchController : SuperController
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public SearchController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Query(string q)
         {
-            return View();
+            // get all invoices for team
+            var query = _dbContext.Invoices
+                .AsQueryable()
+                .Include(i => i.Items)
+                .Include(i => i.Payment)
+                .Where(i => i.Team.Slug == TeamSlug);
+
+            var invoices = query
+                .OrderByDescending(i => i.Id)
+                .ToList();
+
+            var model = new SearchResultsViewModel()
+            {
+                Invoices = invoices,
+            };
+
+            return View(model);
         }
     }
 }
---
+++ src/Payments.Mvc/Models/SearchViewModels/SearchResultsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using Payments.Core.Domain;
+
+namespace Payments.Mvc.Models.SearchViewModels
+{
+    public class SearchResultsViewModel
+    {
+        public IList<Invoice> Invoices { get; set; }
+    }
+}
