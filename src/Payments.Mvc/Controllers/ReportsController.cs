0d9761ffa4a88f56d86300476c6844fb6ecd2de4
John Knoll <jpknoll@gmail.com>
2018-10-02 11:32:14 -0700

    add new report

--- src/Payments.Mvc/Controllers/ReportsController.cs
+++ src/Payments.Mvc/Controllers/ReportsController.cs
@@ -1,13 +1,68 @@
 ﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Payments.Core.Data;
+using Payments.Core.Extensions;
+using Payments.Mvc.Models.ReportViewModels;
+using Payments.Mvc.Models.Roles;
 
 namespace Payments.Mvc.Controllers
 {
+    [Authorize(Policy = PolicyCodes.TeamEditor)]
     public class ReportsController : SuperController
     {
+        private readonly ApplicationDbContext _dbContext;
+
+        public ReportsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult TaxReport()
+        {
+            var lastFiscalYear = DateTime.UtcNow.FiscalYear() - 1;
+
+            var model = new TaxReportViewModel()
+            {
+                FiscalYear = lastFiscalYear,
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult TaxReport(TaxReportViewModel model)
+        {
+            // get all invoices for team
+            var query = _dbContext.Invoices
+                .AsQueryable()
+                .Include(i => i.Items)
+                .Include(i => i.Payment)
+                .Where(i => i.Team.Slug == TeamSlug);
+
+            // for this fiscal year
+            var fiscalStart = new DateTime(model.FiscalYear - 1, 7, 1);
+            var fiscalEnd = new DateTime(model.FiscalYear, 7, 1);
+
+            query = query.Where(i =>
+                   i.Payment.OccuredAt >= fiscalStart
+                && i.Payment.OccuredAt <= fiscalEnd);
+
+            var invoices = query
+                .OrderByDescending(i => i.Id)
+                .ToList();
+
+            model.Invoices = invoices;
+
+            return View(model);
         }
     }
 }
---
+++ src/Payments.Mvc/Models/ReportViewModels/TaxReportViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Payments.Core.Domain;
+
+namespace Payments.Mvc.Models.ReportViewModels
+{
+    public class TaxReportViewModel
+    {
+        [Display(Name = "Fiscal Year")]
+        public int FiscalYear { get; set; }
+
+        public IList<Invoice> Invoices { get; set; }
+    }
+}
