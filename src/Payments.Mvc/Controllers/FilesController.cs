ab9277b31594629f1a1293c24b85d6838ab83b06
John Knoll <jpknoll@gmail.com>
2018-10-04 16:07:00 -0700

    unused

--- src/Payments.Mvc/Controllers/FilesController.cs
+++ src/Payments.Mvc/Controllers/FilesController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Payments.Core.Data;
-using Payments.Core.Domain;
 using Payments.Core.Services;
-using Payments.Mvc.Models.InvoiceViewModels;
 using Payments.Mvc.Models.Roles;
 
 namespace Payments.Mvc.Controllers
@@ -33,22 +29,6 @@ namespace Payments.Mvc.Controllers
             var stream = await blob.OpenReadAsync();
 
             return File(stream, blob.Properties.ContentType, filename);
-
-            //// filter by team
-            //var team = await _dbContext.Teams
-            //    .Include(t => t.Accounts)
-            //    .FirstOrDefaultAsync(t => t.Slug == TeamSlug);
-
-            //var invoiceAttachment = await _dbContext.InvoiceAttachments
-            //    .FirstOrDefaultAsync(a => a.Id == id && a.TeamId == team.Id);
-
-            //if (invoiceAttachment == null)
-            //{
-            //    return NotFound(null);
-            //}
-
-            //var url = await _storageService.GetSharedAccessSignature(invoiceAttachment.Identifier);
-            //return Redirect(url.AccessUrl);
         }
 
         [HttpPost]
