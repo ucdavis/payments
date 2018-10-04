81a206faa23729cc1db8fc012762e830646f25bf
John Knoll <jpknoll@gmail.com>
2018-10-04 16:07:16 -0700

    add table

---
+++ src/Payments.Sql/dbo/Tables/InvoiceAttachments.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[InvoiceAttachments]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL,
+    [Identifier] NVARCHAR(255) NOT NULL, 
+    [FileName] NVARCHAR(255) NOT NULL, 
+    [ContentType] NVARCHAR(50) NOT NULL, 
+    [Size] INT NOT NULL, 
+    [InvoiceId] INT NULL, 
+    [TeamId] INT NULL,
+	CONSTRAINT [PK_InvoiceAttachmentss] PRIMARY KEY CLUSTERED ([Id] ASC),
+)
