6d7aa3e9353237f7f2919e7a15630fd5001fe353
John Knoll <jpknoll@gmail.com>
2018-10-24 09:55:38 -0700

    allow local files

--- src/Payments.Mvc/Startup.cs
+++ src/Payments.Mvc/Startup.cs
@@ -5,6 +5,7 @@ using AspNetCore.Security.CAS;
 using jsreport.AspNetCore;
 using jsreport.Binary;
 using jsreport.Local;
+using jsreport.Types;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -112,7 +113,12 @@ namespace Payments.Mvc
             // add pdf reporting server
             services.AddJsReport(new LocalReporting()
                 .UseBinary(JsReportBinary.GetBinary())
-                .Configure(c => c.FileSystemStore().BaseUrlAsWorkingDirectory())
+                .Configure(c =>
+                {
+                    c.AllowLocalFilesAccess = true;
+                    c.FileSystemStore().BaseUrlAsWorkingDirectory();
+                    return c;
+                })
                 .RunInDirectory(Environment.ContentRootPath)
                 .AsUtility()
                 .Create());
