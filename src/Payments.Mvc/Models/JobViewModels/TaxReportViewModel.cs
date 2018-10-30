ae9f5c0f1070d863a2bdf0d24778492a80baa16d
John Knoll <jpknoll@gmail.com>
2018-10-30 11:50:01 -0700

    add year selector

---
+++ src/Payments.Mvc/Models/JobViewModels/TaxReportViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payments.Mvc.Models.JobViewModels
+{
+    public class TaxReportViewModel
+    {
+        public int Year { get; set; }
+    }
+}
