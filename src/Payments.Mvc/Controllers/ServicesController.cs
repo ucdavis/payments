5dc551dc00620e949b5d42a46ea57cbf4565b998
John Knoll <jpknoll@gmail.com>
2018-10-30 11:24:53 -0700

    services controller that can consume

---
+++ src/Payments.Mvc/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Payments.Mvc.Models.Roles;
+
+namespace Payments.Mvc.Controllers
+{
+    [Authorize(Policy = PolicyCodes.ServiceKey)]
+    public abstract class ServicesController : Controller
+    {
+    }
+}
