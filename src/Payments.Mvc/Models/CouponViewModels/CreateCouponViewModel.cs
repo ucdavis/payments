813318f757eef716178e1035b4ab97e38e1837e6
John Knoll <jpknoll@gmail.com>
2018-11-08 15:53:38 -0800

    add coupon controller and view

---
+++ src/Payments.Mvc/Models/CouponViewModels/CreateCouponViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Payments.Mvc.Models.CouponViewModels
+{
+    public class CreateCouponViewModel
+    {
+        public string Name { get; set; }
+
+        public string Code { get; set; }
+
+        public decimal? DiscountAmount { get; set; }
+
+        public decimal? DiscountPercent { get; set; }
+
+        public DateTime? ExpiresAt { get; set; }
+    }
+}
