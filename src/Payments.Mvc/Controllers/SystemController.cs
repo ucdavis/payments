562e4c94eade8fa4ad2c6ffc49ad554162bd4d36
John Knoll <jpknoll@gmail.com>
2019-02-25 14:27:41 -0800

    create recalculate method

--- src/Payments.Mvc/Controllers/SystemController.cs
+++ src/Payments.Mvc/Controllers/SystemController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Payments.Core.Domain;
 using Payments.Mvc.Models.Roles;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Payments.Core.Data;
 using Payments.Mvc.Identity;
 
 namespace Payments.Mvc.Controllers
@@ -11,11 +14,13 @@ namespace Payments.Mvc.Controllers
     [Authorize(Roles = ApplicationRoleCodes.Admin)]
     public class SystemController : SuperController
     {
+        private readonly ApplicationDbContext _dbContext;
         private readonly ApplicationUserManager _userManager;
         private readonly SignInManager<User> _signInManager;
 
-        public SystemController(ApplicationUserManager userManager, SignInManager<User> signInManager)
+        public SystemController(ApplicationDbContext dbContext, ApplicationUserManager userManager, SignInManager<User> signInManager)
         {
+            _dbContext = dbContext;
             _userManager = userManager;
             _signInManager = signInManager;
         }
@@ -38,6 +43,26 @@ namespace Payments.Mvc.Controllers
             Message = $"Signed in as {id}";
 
             return RedirectToAction("Index", "Home");
+        }
+
+        public async Task<IActionResult> UpdateCalculatedValues()
+        {
+            var invoices = await _dbContext.Invoices
+                .Include(i => i.Coupon)
+                .Include(i => i.Items)
+                .ToListAsync();
+
+            foreach (var invoice in invoices)
+            {
+                invoice.UpdateCalculatedValues();
+            }
+
+            await _dbContext.SaveChangesAsync();
+
+            return new JsonResult(new
+            {
+                success = true,
+            });
         }
     }
 }
