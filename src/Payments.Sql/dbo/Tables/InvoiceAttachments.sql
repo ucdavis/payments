af2c0acabecf48d5bb29aae25fcdf2aed03f661a
Jason Sylvestre <jsylvestre@ucdavis.edu>
2021-01-06 09:32:05 -0800

    Increase content type field from 50 to 255

--- src/Payments.Sql/dbo/Tables/InvoiceAttachments.sql
+++ src/Payments.Sql/dbo/Tables/InvoiceAttachments.sql
@@ -1,11 +1,12 @@
-﻿CREATE TABLE [dbo].[InvoiceAttachments]
-(
-	[Id] INT IDENTITY (1, 1) NOT NULL,
-	[Identifier] NVARCHAR(255) NOT NULL, 
-    [FileName] NVARCHAR(255) NOT NULL, 
-    [ContentType] NVARCHAR(50) NOT NULL, 
-    [Size] BIGINT NOT NULL, 
-    [InvoiceId] INT NOT NULL, 
-    CONSTRAINT [PK_InvoiceAttachments] PRIMARY KEY CLUSTERED ([Id] ASC), 
-    CONSTRAINT [FK_InvoiceAttachments_Invoices_InvoiceId] FOREIGN KEY ([InvoiceId]) REFERENCES [Invoices]([Id]),
-)
+﻿CREATE TABLE [dbo].[InvoiceAttachments] (
+    [Id]          INT            IDENTITY (1, 1) NOT NULL,
+    [Identifier]  NVARCHAR (255) NOT NULL,
+    [FileName]    NVARCHAR (255) NOT NULL,
+    [ContentType] NVARCHAR (255) NOT NULL,
+    [Size]        BIGINT         NOT NULL,
+    [InvoiceId]   INT            NOT NULL,
+    CONSTRAINT [PK_InvoiceAttachments] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_InvoiceAttachments_Invoices_InvoiceId] FOREIGN KEY ([InvoiceId]) REFERENCES [dbo].[Invoices] ([Id])
+);
+
+
