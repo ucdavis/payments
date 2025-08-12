70abca7ac0b09e6c2dc29ae2620c83a994c61b05
Cal <cydoval@mac.com>
2025-08-12 08:38:24 -0700

    first two issues and all the data-dismiss

--- src/Payments.Mvc/ClientApp/src/components/alert.tsx
+++ src/Payments.Mvc/ClientApp/src/components/alert.tsx
@@ -1,23 +1,32 @@
 import * as React from 'react';
 
 interface IProps {
-    className: string;
-    onDismiss: () => void;
+  className: string;
+  onDismiss: () => void;
 }
 
 export default class Alert extends React.Component<IProps, {}> {
-    public render() {
-        const { className, onDismiss } = this.props;
+  public render() {
+    const { className, onDismiss } = this.props;
 
-        return (
-            <div className={"d-flex justify-content-between align-items-center alert " + className} role="alert">
-                <div>
-                    { this.props.children }
-                </div>
-                <button type="button" className="close" aria-label="Close" onClick={onDismiss}>
-                    <i className="fas fa-times" />
-                </button>
-            </div>
-        );
-    }
+    return (
+      <div
+        className={
+          'd-flex justify-content-between align-items-center alert alert-dismissible ' +
+          className
+        }
+        role='alert'
+      >
+        <div>{this.props.children}</div>
+        <button
+          type='button'
+          className='btn-close'
+          aria-label='Close'
+          onClick={onDismiss}
+        >
+          <i className='fas fa-times' />
+        </button>
+      </div>
+    );
+  }
 }
