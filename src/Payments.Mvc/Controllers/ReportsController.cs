2df842273015eba77b955eac0a0b919ef66c5bac
Jason Sylvestre <jsylvestre@ucdavis.edu>
2026-01-06 15:30:37 -0800

    Enhances Recharge Activity Report
    Improves the Recharge Activity Report by including "Entered By", "Paid By", and "Approved By" columns.
    Also includes the corresponding data for these fields.
    
    Adds functionality to display the actor for the "Recharge Paid By Customer" history action.
    Hides the added columns by default and makes them toggleable.

--- src/Payments.Mvc/Controllers/ReportsController.cs
+++ src/Payments.Mvc/Controllers/ReportsController.cs
@@ -70,7 +70,7 @@ namespace Payments.Mvc.Controllers
             //If we wanted to include other history actions, we could add them to the where clause
             var invoiceQuery = _dbContext.Invoices
                 .Include(a => a.RechargeAccounts)
-                .Include(a => a.History.Where(a => a.Type == HistoryActionTypes.RechargeCompletedInSloth.TypeCode))
+                .Include(a => a.History.Where(a => a.Type == HistoryActionTypes.RechargeCompletedInSloth.TypeCode || a.Type == HistoryActionTypes.RechargePaidByCustomer.TypeCode))
                 .Where(i => i.Team.Slug == TeamSlug && i.Type == InvoiceTypes.Recharge);
             if (year.HasValue)
             {
